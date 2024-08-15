39cc1cbdcf610d9630521a7ca4993c71de5256ad
Akash Gupta <akash.g6787@gmail.com>
2024-08-16 02:08:32 +0530

    corrected-message-boxes

--- static/script.js
+++ static/script.js
@@ -1,30 +1,62 @@
 function getBotResponse() {
-    var rawText = $("#textInput").val();
-    var userHtml = '<p class="userText"><span>' + rawText + "</span></p>";
-    $("#textInput").val("");
-    $("#chatbox").append(userHtml);
+  var rawText = $("#textInput").val();
+  
+  // Display user's text
+  var userHtml = '<p class="userText"><span>' + rawText + "</span></p>";
+  $("#textInput").val("");
+  $("#chatbox").append(userHtml);
+  document
+    .getElementById("userInput")
+    .scrollIntoView({ block: "start", behavior: "smooth" });
+
+  // Create a placeholder for the bot response
+  var botHtml = '<p class="botText"><span id="botResponse"></span></p>';
+  $("#chatbox").append(botHtml);
+
+  // Establish an EventSource connection to stream responses
+  var source = new EventSource("/get?msg=" + encodeURIComponent(rawText));
+  var isResponseComplete = false;
+
+  source.onmessage = function (event) {
+    // Accumulate the response in the same message box
+    var currentText = $("#botResponse").html();
+    $("#botResponse").html(currentText + event.data);
     document
       .getElementById("userInput")
       .scrollIntoView({ block: "start", behavior: "smooth" });
-  
-    var source = new EventSource("/get?msg=" + encodeURIComponent(rawText));
-  
+  };
+
+  source.onerror = function () {
+    source.close();
+    if (!isResponseComplete) {
+      // Mark the end of the response
+      $("#botResponse").attr("id", "");
+      isResponseComplete = true;
+    }
+  };
+
+  source.onopen = function () {
+    // Once the connection is open, make sure to handle it
     source.onmessage = function (event) {
-      var botHtml = '<p class="botText"><span>' + event.data + "</span></p>";
-      $("#chatbox").append(botHtml);
+      // Append the data to the existing response box
+      var currentText = $("#botResponse").html();
+      $("#botResponse").html(currentText + event.data);
       document
         .getElementById("userInput")
         .scrollIntoView({ block: "start", behavior: "smooth" });
     };
-  
+
     source.onerror = function () {
+      // When the stream ends, mark the response as complete
+      $("#botResponse").attr("id", "");
       source.close();
+      isResponseComplete = true;
     };
+  };
+}
+
+$("#textInput").keypress(function (e) {
+  if (e.which == 13) {
+    getBotResponse();
   }
-  
-  $("#textInput").keypress(function (e) {
-    if (e.which == 13) {
-      getBotResponse();
-    }
-  });
-  
+});
